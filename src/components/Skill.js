b5239dc8c2292962535dde06755d670511c22d33
Gwennie Lau <geeeedev@gmail.com>
2020-12-11 21:26:15 -0800

    code clean up

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,44 +1,43 @@
 import React from "react";
 
-const Skill = ({skillDataObj}) => {
-    return (
-        <>
-        <h5>Frontend:</h5>
-        {skillDataObj.frontend.map((each)=>
-            <p>{each}</p>
-        )}
-<h5>Backend:</h5>
-        {skillDataObj.backend.map((each)=>
-            <p>{each}</p>
-        )}
+const Skill = ({ skillDataObj }) => {
+  return (
+    <>
+      <h5>Frontend:</h5>
+      {skillDataObj.frontend.map((each) => (
+        <p>{each}</p>
+      ))}
+      <h5>Backend:</h5>
+      {skillDataObj.backend.map((each) => (
+        <p>{each}</p>
+      ))}
 
-<h5>Framework:</h5>
-        {skillDataObj.framework.map((each)=>
-            <p>{each}</p>
-        )}
+      <h5>Framework:</h5>
+      {skillDataObj.framework.map((each) => (
+        <p>{each}</p>
+      ))}
 
-<h5>Database:</h5>
-        {skillDataObj.database.map((each)=>
-            <p>{each}</p>
-        )}
+      <h5>Database:</h5>
+      {skillDataObj.database.map((each) => (
+        <p>{each}</p>
+      ))}
 
-<h5>Deployment:</h5>
-        {skillDataObj.deployment.map((each)=>
-            <p>{each}</p>
-        )}
+      <h5>Deployment:</h5>
+      {skillDataObj.deployment.map((each) => (
+        <p>{each}</p>
+      ))}
 
-<h5>Tool:</h5>
-        {skillDataObj.tool.map((each)=>
-            <p>{each}</p>
-        )}
- 
-    <h5>Version Control:</h5>
-        {skillDataObj.versionControl.map((each)=>
-            <p>{each}</p>
-        )}
+      <h5>Tool:</h5>
+      {skillDataObj.tool.map((each) => (
+        <p>{each}</p>
+      ))}
 
-        </>
-    );
+      <h5>Version Control:</h5>
+      {skillDataObj.versionControl.map((each) => (
+        <p>{each}</p>
+      ))}
+    </>
+  );
 };
 
 export default Skill;

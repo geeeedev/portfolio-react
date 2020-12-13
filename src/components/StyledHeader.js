79cbc8dd0fff0c3c8fb4675fbb1d13e7caf1c7f3
Gwennie Lau <geeeedev@gmail.com>
2020-12-13 14:44:32 -0800

    clean up Nav Header

--- src/components/StyledHeader.js
+++ src/components/StyledHeader.js
@@ -4,64 +4,77 @@ export const Img = styled.img`
   width: 100vw;
   height: 60vh;
   opacity: 0.5;
+  margin-bottom: -3px;
 
+  // outline: 2px dotted yellow;
+`;
+
+export const HambgBar = styled.img`
+  width: auto;
+  height: 3.5vh;
+
+  //   position: relative;
+  //   right: -20px;
   // outline: 2px dotted yellow;
 `;
 
 export const Header = styled.header`
   background-color: #282c34;
-  // background-color: #333944;
-  // background-color: #3f4757;
-  height: 3vh;
+  background-color: #303137; 
+  height: 3.5vh;
+  padding-right: 15px;
+  margin: 0px;
+  color: #7d6c53;
+  font-size: calc(10px + 2vmin);
   display: flex;
   flex-direction: column;
   align-items: flex-end;
   // justify-content: flex-end;  // N/A
   // overflow: hidden;           // N/A
-  font-size: calc(10px + 2vmin);
-  color: white;
   position: sticky;
   position: -webkit-sticky; //Safari
   top: 0;
   &:hover {
-    font-weight: bold;
-    color: yellow;
+    color: #61dafb;
+    font-weight: 500;
   }
 
-  outline: 2px dotted red;
+  // outline: 2px dotted red;
 `;
 
 export const NavLink = styled.a`
   color: #61dafb; //lightBlue
   text-decoration: none;
-  &:hover {
-    background-color: #61dafb; //swap color
-    color: #282c34;
-    font-weight: bold;
-  }
-
-  //NavLink shows when Header is hovered
   display: none;
+  // NavLink shows when Header is hovered
   ${Header}:hover & {
     display: block;
   }
+  // styles when each NavLink is hovered
+  &:hover {
+    background-color: #61dafb; //swap color
+    color: #282c34;
+    font-weight: 900;
+  }
 `;
 
-// //plain CSS version
-// /* Style the links inside the header navigation */
+// //plain CSS version on NavLink
+// //Style the links inside the header navigation
 // header a {
-//     display: block;
 //     color: #61dafb;   //lightBlue
 //     text-decoration: none;
+//     display: none;
 // }
 
-// /* Change the color of links on hover */
+// //Links show when header is hovered
+// header:hover  a {
+//    display: block;
+//}
+
+// //Change the color of links on hover
 // header a:hover {
-//     bbackground-color: #61dafb; //switched
-//     color: #282c34;
-//     font-weight: bold;
+//      background-color: #61dafb; //swap color
+//      color: #282c34;
+//      font-weight: 900;
 // }
 
-export const NavLinkIcon = styled(NavLink)`
-  display: none;
-`;

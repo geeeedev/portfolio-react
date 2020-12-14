78225b2bb312406c82caf4df48f769fc3c763f33
Gwennie Lau <geeeedev@gmail.com>
2020-12-14 12:48:41 -0800

    Added Social Media Icons

---
+++ src/components/StyledSocialMediaIcon.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const SocialMediaIcons = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center; //vertical align
+  justify-content: center; //horizontal align
+  height: 80px;
+  width: 400px;
+
+  position: relative;
+  left: 30%;
+
+  outline: 3px dashed red;
+`;

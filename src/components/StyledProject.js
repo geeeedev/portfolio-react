8929a51dd72fa907926d0e44aeeba807001a01c8
Gwennie Lau <geeeedev@gmail.com>
2020-12-15 21:02:37 -0800

    update project comp

--- src/components/StyledProject.js
+++ src/components/StyledProject.js
@@ -7,18 +7,15 @@ import styled from "styled-components";
 
 // per project container
 export const Row = styled.div`
+  padding: 100px 150px;
   display: flex;
+  // flex-wrap: wrap;
   // justify-content: ${(props) => props.justify};  //N/A
   justify-content: ${({ justify }) => justify || "space-evenly"};
-  flex-wrap: wrap;
-  margin: 0px 120px;
-  padding: 15px;
   // box-shadow: 0px 0px 4px lightblue;
-  position: relative;
-  // right: 30%;
 
   //transition and delay here
-  // outline: 1px solid blue;
+  outline: 1px solid blue;      //////////// Row - blue
 `;
 
 // per project item: img vs. desc
@@ -26,30 +23,33 @@ export const Col = styled.div`
   flex: 1;
   order: ${({ idx }) => (idx % 2 === 0 ? "2" : "1")};
   position: relative;
-  // z-index: -1;
   &:hover {
-    z-index: 1;
+    background-color: olive;    ///////////// Test
   }
-
-  outline: 1px dashed green;
+  
+  // z-index: -1;  //canNOT use -1, -1 kills onClick
+  outline: 1px dashed green;    //////////// Col - green dash
 `;
 
 export const ImgProj = styled.img`
-  // position: absolute;
-  // top: 0;
-  // right: 0;
   width: 100%;
-  height: 100%;
+  height: 95%;
+  position: relative;
+  &:hover {
+    opacity: 0.5;
+  }
+  
   tooltip: "click for live project";
-`;
+  `;
 
 export const Tooltip = styled.span`
-visibility: hidden;
-color: red;
-text-align: center;
-position: absolute;
-z-index: 2;
+  visibility: hidden;
+  color: red;
+  text-align: center;
+  position: absolute;
+  z-index: 2;
 
-${ImgProj}:hover & {
-  visibility: visible;
-}`;
+  ${ImgProj}:hover & {
+    visibility: visible;
+  }
+`;

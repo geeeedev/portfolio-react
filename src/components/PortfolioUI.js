2f14cf20b187847241f1e554a108c7747cd34be8
Gwennie Lau <geeeedev@gmail.com>
2020-12-16 19:19:46 -0800

    added Proj Container

--- src/components/PortfolioUI.js
+++ src/components/PortfolioUI.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ProjContainer } from "./StyledPortfolioUI"
 import Projects from "../data/Projects.json";
 import Project from "./Project";
 import Skills from "../data/Skills.json";
@@ -6,13 +7,16 @@ import Skill from "./Skill";
 
 // import SocialMediaIconList from "./SocialMediaIconList";
 
-const PortfolioUI = () => {
+const PortfolioUI = () => {  
+  //This should really be ProjectSection
   return (
     <>
       {/* <Skill skillDataObj={Skills} /> */}
+    <ProjContainer> 
       {Projects.map((proj, idx) => (
         <Project projDataObj={proj} idx={idx}/>
       ))}
+    </ProjContainer> 
       {/* <SocialMediaIconList /> */}
     </>
   );

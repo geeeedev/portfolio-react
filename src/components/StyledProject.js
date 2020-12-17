7addb381c490409c4d678f9fbad156c58d455db2
Gwennie Lau <geeeedev@gmail.com>
2020-12-16 19:21:50 -0800

    Tweaking UI

--- src/components/StyledProject.js
+++ src/components/StyledProject.js
@@ -5,9 +5,9 @@ import styled from "styled-components";
 //   outline: 1px dashed red;
 // `;
 
-// per project container
+// per project 
 export const Row = styled.div`
-  padding: 100px 150px;
+  padding: 0px 150px;
   display: flex;
   // flex-wrap: wrap;
   // justify-content: ${(props) => props.justify};  //N/A
@@ -15,7 +15,7 @@ export const Row = styled.div`
   // box-shadow: 0px 0px 4px lightblue;
 
   //transition and delay here
-  outline: 1px solid blue;      //////////// Row - blue
+  // outline: 1px solid blue; //////////// Row - blue
 `;
 
 // per project item: img vs. desc
@@ -24,32 +24,45 @@ export const Col = styled.div`
   order: ${({ idx }) => (idx % 2 === 0 ? "2" : "1")};
   position: relative;
   &:hover {
-    background-color: olive;    ///////////// Test
+    background-color: olive; ///////////// Test
   }
-  
+
   // z-index: -1;  //canNOT use -1, -1 kills onClick
-  outline: 1px dashed green;    //////////// Col - green dash
+  // outline: 1px dashed green; //////////// Col - green dash
+`;
+
+export const RepoButton = styled.button`
+  color: #61dafb;
 `;
 
 export const ImgProj = styled.img`
   width: 100%;
-  height: 95%;
+  height: 100%;
   position: relative;
   &:hover {
     opacity: 0.5;
   }
-  
-  tooltip: "click for live project";
-  `;
 
-export const Tooltip = styled.span`
-  visibility: hidden;
-  color: red;
-  text-align: center;
-  position: absolute;
-  z-index: 2;
-
-  ${ImgProj}:hover & {
-    visibility: visible;
-  }
+  // tooltip: "click for live project";
 `;
+
+export const DescProj = styled.div`
+  padding: 100px;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+
+  outline: 1px dotted pink;
+`;
+
+// export const Tooltip = styled.span`
+//   visibility: hidden;
+//   color: red;
+//   text-align: center;
+//   position: absolute;
+//   z-index: 2;
+
+//   ${ImgProj}:hover & {
+//     visibility: visible;
+//   }
+// `;

4f677c9b151e81d7d4c44c65c1e385ff67270307
Gwennie Lau <geeeedev@gmail.com>
2020-12-21 23:19:38 -0800

    code clean up

--- src/components/PortfolioUI.js
+++ src/components/PortfolioUI.js
@@ -2,22 +2,19 @@ import React from "react";
 import { ProjContainer } from "./StyledPortfolioUI"
 import Projects from "../data/Projects.json";
 import Project from "./Project";
-import Skills from "../data/Skills.json";
-import Skill from "./Skill";
+
 
 // import SocialMediaIconList from "./SocialMediaIconList";
 
 const PortfolioUI = () => {  
-  //This should really be ProjectSection
+  //This should really be ProjectSection or ProjectListing
   return (
     <>
-      {/* <Skill skillDataObj={Skills} /> */}
     <ProjContainer> 
       {Projects.map((proj, idx) => (
         <Project projDataObj={proj} idx={idx}/>
       ))}
     </ProjContainer> 
-      {/* <SocialMediaIconList /> */}
     </>
   );
 };

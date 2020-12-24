1a858bb233d2e84ce0a18670512035e2d05855f2
Gwennie Lau <geeeedev@gmail.com>
2020-12-23 22:41:57 -0800

    working on Social Icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,47 @@
 import React, { useState } from "react";
-import { Img, HambgBar, Header, NavLink } from "./StyledHeader";
+import {
+  Img,
+  HeaderSMIconList,
+  HambgBar,
+  Header,
+  NavLink,
+} from "./StyledHeader";
 import HeaderImg from "../img/HeaderImg.png";
 import NavHambg from "../img/NavHambgGray.png";
 import CV from "../data/GwennieLauResume.pdf";
+
+import SocialMediaIcons from "./SocialMediaIcons";
 
 import Modal from "react-bootstrap/Modal";
 /* Import bottstrap css - can be included here or in src/index.js or App.js INSTEAD OF index.html <link> */
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NavHeader = () => {
-
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
       <Img src={HeaderImg} alt="Header Image - Griffith Park Sunset" />
       <Header>
-      {/* <SocialMediaIconList />  {/* float left? */}
+        <HeaderSMIconList>
+          {/* <SocialMediaIcons padSize="0.5em"/> */}
+          <SocialMediaIcons padSize="0.25em" />
+        </HeaderSMIconList>
         <HambgBar src={NavHambg} alt="" />
         <NavLink href="#proj"> Projects </NavLink>
         <NavLink href="#skill"> Skills </NavLink>
-        {/* <NavLink href="#about"> About </NavLink> */}
         <NavLink href="#contact"> Contact </NavLink>
         <NavLink href="#resume" onClick={() => setShowModal(true)}>
           Resume
         </NavLink>
+        {/* <NavLink href="#about"> About </NavLink> */}
       </Header>
 
       <Modal
         show={showModal}
         size="lg"
         onHide={() => setShowModal(false)}
-        aria-labelledby="Gwennie's Resume in PDF"
+        // aria-labelledby="Gwennie's Resume in PDF"
       >
         {/* <Modal.Header closeButton>
           <Modal.Title> Gwennie's Resume </Modal.Title>
@@ -42,6 +52,7 @@ const NavHeader = () => {
             data={CV}
             width="100%"
             height="1000"
+            aria-labelledby="Gwennie's Resume in PDF"
           ></object>
         </Modal.Body>
       </Modal>

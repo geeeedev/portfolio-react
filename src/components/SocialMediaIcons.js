e017c3a3ac0425b028ab7601f0a393344494d4c4
Gwennie Lau <geeeedev@gmail.com>
2020-12-23 22:43:49 -0800

    reorg'd code

---
+++ src/components/SocialMediaIcons.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { AnimatedSocialIcon } from "react-animated-social-icons";
+// import { SocialMediaIconList } from "./StyledSocialMediaIconList";
+
+const SocialMediaIcons = ({ padSize }) => {
+  return (
+    <>
+      <AnimatedSocialIcon
+        brandName="github"
+        url="https://github.com/geeeedev"
+        defaultColor="#b6c3d9" //orig
+        // hoverColor="#15202B"
+        hoverColor="#223344"
+        hoverColor="darkred"
+        animation="grow"
+        animationDuration={1}
+        width="2em"
+        style={{ paddingRight: `${padSize}` }}
+        newPage={true}
+      />
+      <AnimatedSocialIcon
+        brandName="twitter"
+        url="https://twitter.com/geeeedev"
+        defaultColor="#b6c3d9"
+        hoverColor="#1DA1F2"
+        animation="bounce"
+        animationDuration={1}
+        width="2em"
+        // style={{ padding: "3em", margin: '5%', outline: "1px dashed green" }}
+        style={{ padding: `${padSize}` }}
+        newPage={true}
+      />
+      {/* <AnimatedSocialIcon
+          brandName="linkedin"
+          url="https://www.linkedin.com/in/gwennielau/"
+          defaultColor="#b6c3d9"
+          hoverColor="#0A66C2"
+          animation="float"
+          animationDuration={1}
+          width="2em"
+          // style={{ padding: "3em" }}
+          style={{ padding: `${padSize}` }}
+          newPage={true}
+        /> */}
+    </>
+  );
+};
+
+export default SocialMediaIcons;
---
+++ src/components/StyledFooter.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const FooterSMIconList = styled.footer`
+  margin-top: 3%; 
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  align-items: center; //vertical align
+  justify-content: center; //horizontal align
+
+  // outline: 1px dashed orange;
+`;
+
+export const CopyRight = styled.p`
+  margin-top: 10px;
+  margin-bottom: 50px;
+  text-align: center;
+  font-size: smaller;
+
+  // outline: 1px dashed pink;
+`;

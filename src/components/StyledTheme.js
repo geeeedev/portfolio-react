67e203443d42524269dbfdf64f2f5a8426eb258a
Gwennie Lau <geeeedev@gmail.com>
2020-12-27 23:57:44 -0800

    working on theme colors

--- src/components/StyledTheme.js
+++ src/components/StyledTheme.js
@@ -1,16 +1,18 @@
 
 const colorRhino = "#3f4757";
-// const colorWhite = "whitesmoke";
-const colorWhite = "lightblue";
+const colorWhite = "whitesmoke";
+// const colorWhite = "lightblue";
 
 export const darkTheme = {
     bgColor: colorRhino,
     txtColor: colorWhite,
-    btnColor: "lime"
+    lnkColor: "#61dafb",
+    btnColor: "#61dafb"
 };
 
 export const lightTheme = {
     bgColor: colorWhite,
     txtColor: colorRhino,
+    lnkColor: "darkred",
     btnColor: "olive"
 };

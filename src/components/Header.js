c41d03d72e9a0749536c6e6794f39d84921280dc
Gwennie Lau <geeeedev@gmail.com>
2020-12-31 22:56:01 -0800

    working on carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,16 @@ import {
   ModeSwitch,
   NavLink,
 } from "./StyledHeader";
-import HeaderImg from "../img/HeaderImg.png";
+import HeaderImg0 from "../img/HeaderImg0z.png";
+import HeaderImg1 from "../img/HeaderImg1.png";
+import HeaderImg2 from "../img/HeaderImg2.png";
+import HeaderImg3 from "../img/HeaderImg3.png";
+import HeaderImg4 from "../img/HeaderImg4.png";
+import HeaderImg5 from "../img/HeaderImg5.png";
+import HeaderImg6 from "../img/HeaderImg6.png";
+import HeaderImg7 from "../img/HeaderImg7.png";
+import HeaderImg8 from "../img/HeaderImg8.png";
+import HeaderImg9 from "../img/HeaderImg9.png";
 import MoonIcon from "../img/IconMoon2.png";
 import SunIcon from "../img/IconSun2.ico";
 import CV from "../data/GwennieLauResume.pdf";
@@ -14,35 +23,78 @@ import CV from "../data/GwennieLauResume.pdf";
 import SocialMediaIcons from "./SocialMediaIcons";
 
 import Modal from "react-bootstrap/Modal";
+import Carousel from "react-bootstrap/Carousel";
 /* Import bottstrap css - can be included here or in src/index.js or App.js INSTEAD OF index.html <link> */
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-const NavHeader = ({dkMode, setDkMode}) => {
+const NavHeader = ({ dkMode, setDkMode }) => {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <Img src={HeaderImg} alt="Header Image - Griffith Park Sunset" />
+      <Carousel indicators={false} slide interval={5000} >
+        <Carousel.Item interval={7000} >
+          <Img src={HeaderImg0} alt="Header Image - Griffith Park Sunset" />
+        </Carousel.Item>
+
+        <Carousel.Item >
+          <Img src={HeaderImg1} alt="Header Image - Griffith Park Sunset" />
+        </Carousel.Item>
+
+        <Carousel.Item >
+          <Img src={HeaderImg2} alt="Header Image - Griffith Park Sunset" />
+        </Carousel.Item>
+
+        <Carousel.Item >
+          <Img src={HeaderImg3} alt="Header Image - Griffith Park Sunset" />
+        </Carousel.Item>
+
+        <Carousel.Item >
+          <Img src={HeaderImg4} alt="Header Image - Griffith Park Sunset" />
+        </Carousel.Item>
+
+        <Carousel.Item >
+          <Img src={HeaderImg5} alt="Header Image - Griffith Park Sunset" />
+        </Carousel.Item>
+
+        <Carousel.Item >
+          <Img src={HeaderImg6} alt="Header Image - Griffith Park Sunset" />
+        </Carousel.Item>
+
+        <Carousel.Item >
+          <Img src={HeaderImg7} alt="Header Image - Griffith Park Sunset" />
+        </Carousel.Item>
+
+        <Carousel.Item >
+          <Img src={HeaderImg8} alt="Header Image - Griffith Park Sunset" />
+        </Carousel.Item>
+        
+        <Carousel.Item >
+          <Img src={HeaderImg9} alt="Header Image - Griffith Park Sunset" />
+        </Carousel.Item>
+      </Carousel>
 
       <HeaderSMIconList>
         <SocialMediaIcons padSize="0.25em" />
       </HeaderSMIconList>
       <NavLinkContainer>
-        <ModeSwitch src={dkMode?SunIcon:MoonIcon} alt="Theme Switching Icon" onClick={()=>{setDkMode(!dkMode)}}  />
+        <ModeSwitch
+          src={dkMode ? SunIcon : MoonIcon}
+          alt="Theme Switching Icon"
+          onClick={() => {
+            setDkMode(!dkMode);
+          }}
+        />
         <NavLink href="#proj"> Projects </NavLink>
         <NavLink href="#skill"> Skills </NavLink>
         {/* <NavLink href="#contact"> Contact </NavLink> */}
         <NavLink href="#resume" onClick={() => setShowModal(true)}>
           Resume
         </NavLink>
-        {/* <NavLink href="#about"> About </NavLink> */}
+        {/* <NavLink href="#about"> Find Me </NavLink> */}
       </NavLinkContainer>
 
-      <Modal
-        show={showModal}
-        size="lg"
-        onHide={() => setShowModal(false)}
-      >
+      <Modal show={showModal} size="lg" onHide={() => setShowModal(false)}>
         {/* <Modal.Header closeButton>
           <Modal.Title> Gwennie's Resume </Modal.Title>
         </Modal.Header> */}

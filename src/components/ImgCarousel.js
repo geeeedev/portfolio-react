caca1cce4cea7ca60b77781d5112cedf67a3a80e
Gwennie Lau <geeeedev@gmail.com>
2021-01-03 23:21:01 -0800

    code clean up

---
+++ src/components/ImgCarousel.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Img } from "./StyledHeader";
+import Carousel from "react-bootstrap/Carousel";
+/* Import bottstrap css - can be included here or in src/index.js or App.js INSTEAD OF index.html <link> */
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+const headerImages = {};
+const importAllImgs = (r) => {
+  // console.log(r);
+  r.keys().forEach(
+    (filename) => (headerImages[filename.replace("./", "")] = r(filename))
+    //(filename) => (headerImages[filename] = r(filename)) // ./HeaderImg##.png: Module {}
+  );
+};
+importAllImgs(require.context("../img/", false, /^\.\/HeaderImg\d\d\.png$/));
+//importAllImgs(require.context("../img/", false, /\.png$/));
+
+// console.log(`headerImages`, headerImages);
+// console.log(`imgKeys`, Object.keys(headerImages));
+/** expected obj
+ * headerImages = {
+ *  "HeaderImg00.png": Module { default:"...imgPath" },
+ *  "HeaderImg01.png": Module { default:"...imgPath" },
+ *  "HeaderImg02.png": Module { default:"...imgPath" },
+ *  "HeaderImg03.png": Module { default:"...imgPath" },
+ * }
+ */
+
+const ImgCarousel = () => {
+  return (
+    <Carousel indicators={false} fade interval={5000}>
+      {Object.keys(headerImages).map((img, i) => (
+        <Carousel.Item key={i}>
+          <Img src={headerImages[img].default} alt="Header Photo Carousel" />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default ImgCarousel;

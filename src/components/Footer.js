9631dce8dd36105327e6be119b778bc5aeb6374c
Gwennie Lau <geeeedev@gmail.com>
2021-01-08 15:15:11 -0800

    code refactored / clean up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,10 @@ import React from "react";
 import SocialMediaIcons from "./SocialMediaIcons";
 import { FooterSMIconList, CopyRight } from "./StyledFooter";
 
+const year = new Date().getFullYear(); //(reusable outside of comp)
+
 const Footer = () => {
-  const year = new Date().getFullYear();
+  // const year = new Date().getFullYear();
   return (
     <>
       <FooterSMIconList id="contact">

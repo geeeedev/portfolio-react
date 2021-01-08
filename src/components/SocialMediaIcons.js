4a8f8f62f86fe10268cc06f9fb00c5caa54e2680
Gwennie Lau <geeeedev@gmail.com>
2021-01-08 15:17:00 -0800

    code refactored / clean up

--- src/components/SocialMediaIcons.js
+++ src/components/SocialMediaIcons.js
@@ -1,44 +1,45 @@
 import React from "react";
 import { AnimatedSocialIcon } from "react-animated-social-icons";
-// import { SocialMediaIconList } from "./StyledSocialMediaIconList";
 
+const defaultColor="#b6c3d9";
+const duration=1;
+const iconWidth = "calc(7px + 3vmin)";   //"2em"/"2vw"/"calc(12px + 2vmin)"
 const SocialMediaIcons = ({ padSize }) => {
   return (
     <>
       <AnimatedSocialIcon
         brandName="github"
         url="https://github.com/geeeedev"
-        defaultColor="#b6c3d9" 
+        defaultColor={defaultColor} 
         // hoverColor="#15202B"
         // hoverColor="#223344"
         hoverColor="Chocolate"
         animation="grow"
-        animationDuration={1}
-        width="2vw"
+        animationDuration={duration}
+        width={iconWidth}
         style={{ paddingRight: `${padSize}`}}
         newPage={true}
       />
       <AnimatedSocialIcon
         brandName="twitter"
         url="https://twitter.com/geeeedev"
-        defaultColor="#b6c3d9"
+        defaultColor={defaultColor}
         hoverColor="#1DA1F2"
         animation="bounce"
-        animationDuration={1}
-        width= "2vw"
-        // style={{ padding: "3em", margin: '5%', outline: "1px dashed green" }}
+        animationDuration={duration}
+        width={iconWidth}
         style={{ padding: `${padSize}`}}
         newPage={true}
       />
       {/* <AnimatedSocialIcon
           brandName="linkedin"
           url="https://www.linkedin.com/in/gwennielau/"
-          defaultColor="#b6c3d9"
+          defaultColor={defaultColor}
           hoverColor="#0A66C2"
           animation="float"
-          animationDuration={1}
-          width="2em"
-          // style={{ padding: "3em" }}
+          animationDuration={duration}
+          width={iconWidth}
+          // style={{ padding: "3em", margin: '5%', outline: "1px dashed green" }}
           style={{ padding: `${padSize}` }}
           newPage={true}
         /> */}

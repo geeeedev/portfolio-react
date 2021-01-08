e5258cdf416ecd1d59b82d3bb7374c53787fe598
Gwennie Lau <geeeedev@gmail.com>
2021-01-07 17:20:49 -0800

    optimized images

--- src/components/ImgCarousel.js
+++ src/components/ImgCarousel.js
@@ -12,7 +12,7 @@ const importAllImgs = (r) => {
     //(filename) => (headerImages[filename] = r(filename)) // ./HeaderImg##.png: Module {}
   );
 };
-importAllImgs(require.context("../img/", false, /^\.\/HeaderImg\d\d\.png$/));
+importAllImgs(require.context("../img/", false, /^\.\/HeaderImg\d\d\.jpg$/));
 //importAllImgs(require.context("../img/", false, /\.png$/));
 
 // console.log(`carouselImages`, carouselImages);
@@ -31,7 +31,10 @@ const ImgCarousel = () => {
     <Carousel indicators={false} fade interval={5000}>
       {Object.keys(carouselImages).map((imgName, i) => (
         <Carousel.Item key={i}>
-          <Img src={carouselImages[imgName].default} alt="Header Carousel Photo" />
+          <Img
+            src={carouselImages[imgName].default}
+            alt="Header Carousel Photo"
+          />
         </Carousel.Item>
       ))}
     </Carousel>
